6dd6df90072f397f751d8234d0b7363c43d6cb58
Taisuke Shimoaka <Taisuke.Shimoaka@i3-digital.com>
2020-10-29 08:18:09 +0900

    add support for 'add' & 'sub'.

--- 9cc.c
+++ 9cc.c
@@ -11,7 +11,23 @@ int main(int argc, char **argv)
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+
+    char *p = argv[1];
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+    while (*p != '\0') {
+        if (*p == '+') {
+            printf("  add rax, %ld\n", strtol(p+1, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            printf("  sub rax, %ld\n", strtol(p+1, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "unexpectd charctor!: %c\n", *p);
+        return 2;
+    }
     printf("  ret\n");
     return 0;
 }
